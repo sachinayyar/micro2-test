c61a2e62a3092d0a559ecfb0d0e0433fecc99566
Sachin Ayyar <91127350+sachinayyar@users.noreply.github.com>
2023-06-19 15:35:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,51 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Cloth Products</title>
-    <style>
-        .product {
-            width: 200px;
-            display: inline-block;
-            margin: 10px;
-        }
-        .product img {
-            width: 100%;
-            height: auto;
-        }
-        .product p {
-            text-align: center;
-            font-weight: bold;
-        }
-    </style>
+    <title>Simple Calculator</title>
 </head>
 <body>
-    <?php
-    // Array of cloth products
-    $products = array(
-        array(
-            'name' => 'T-Shirt',
-            'image' => 'https://images.pexels.com/photos/1335077/pexels-photo-1335077.jpeg?cs=srgb&dl=automobile-automotive-car-1335077.jpg&fm=jpg',
-            'price' => 19.99
-        ),
-        array(
-            'name' => 'Jeans',
-            'image' => 'C:\Users\integra\Pictures\architectue.PNG',
-            'price' => 49.99
-        ),
-        array(
-            'name' => 'Dress',
-            'image' => 'dress.jpg',
-            'price' => 79.99
-        )
-    );
-
-    // Displaying products
-    foreach ($products as $product) {
-        echo '<div class="product">';
-        echo '<img src="images/' . $product['image'] . '" alt="' . $product['name'] . '">';
-        echo '<p>' . $product['name'] . ' - $' . $product['price'] . '</p>';
-        echo '</div>';
-    }
-    ?>
+    <form method="post" action="calculator.php">
+        <label>Number 1:</label>
+        <input type="number" name="num1" required><br><br>
+        
+        <label>Number 2:</label>
+        <input type="number" name="num2" required><br><br>
+        
+        <label>Operator:</label>
+        <select name="operator">
+            <option value="+">Addition</option>
+            <option value="-">Subtraction</option>
+            <option value="*">Multiplication</option>
+            <option value="/">Division</option>
+        </select><br><br>
+        
+        <input type="submit" value="Calculate">
+    </form>
 </body>
 </html>
