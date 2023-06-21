e7cadd83c2fdd41804d1dfea5b79748e5e45f078
Sachin Ayyar <91127350+sachinayyar@users.noreply.github.com>
2023-06-21 15:36:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -158,7 +158,7 @@
         var cartButton = document.getElementById("cart-button");
 
         loginButton.addEventListener("click", function() {
-        window.location.href = "http://microservices-demo-my-gateway-525eca1d5089dbdc-istio-system.apps.cluster-sbj27.sbj27.sandbox2370.opentlc.com/loginpage";
+        window.location.href = "http://microservices-demo-my-gateway-525eca1d5089dbdc-istio-system.apps.ocp4.imss.com/loginpage";
             alert("Login button clicked!");
         });
 
