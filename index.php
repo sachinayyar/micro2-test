828aef5302e952e4170c85662cb5eb8c37638d94
Sachin Ayyar <91127350+sachinayyar@users.noreply.github.com>
2023-06-21 12:12:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,108 +3,132 @@
 <head>
     <title>Shopping Application</title>
     <style>
-      /* Style for the overall page layout */
-body {
-    font-family: Arial, sans-serif;
-    margin: 0;
-    padding: 20px;
-}
+        /* Style for the overall page layout */
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 20px;
+        }
 
-h1 {
-    color: #333;
-}
+        h1 {
+            color: #333;
+        }
 
-h2 {
-    color: #666;
-}
+        h2 {
+            color: #666;
+        }
 
-ul {
-    list-style-type: none;
-    padding: 0;
-}
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
 
-li {
-    margin-bottom: 10px;
-}
+        li {
+            margin-bottom: 10px;
+        }
 
-/* Style for product listing */
-.product {
-    display: inline-block;
-    margin-right: 20px;
-    text-align: center;
-}
+        /* Style for product listing */
+        .product {
+            display: inline-block;
+            margin-right: 20px;
+            text-align: center;
+        }
 
-.product img {
-    width: 200px;
-    height: 200px;
-    object-fit: cover;
-    margin-bottom: 10px;
-}
+        .product img {
+            width: 200px;
+            height: 200px;
+            object-fit: cover;
+            margin-bottom: 10px;
+        }
 
-.product h3 {
-    margin: 0;
-    font-size: 18px;
-}
+        .product h3 {
+            margin: 0;
+            font-size: 18px;
+        }
 
-.product p {
-    margin: 5px 0;
-}
+        .product p {
+            margin: 5px 0;
+        }
 
-.product button {
-    background-color: #4CAF50;
-    color: white;
-    border: none;
-    padding: 8px 16px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 14px;
-    cursor: pointer;
-    transition-duration: 0.4s;
-}
+        .product button {
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            padding: 8px 16px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 14px;
+            cursor: pointer;
+            transition-duration: 0.4s;
+        }
 
-.product button:hover {
-    background-color: #45a049;
-}
+        .product button:hover {
+            background-color: #45a049;
+        }
 
-/* Style for the shopping cart section */
-#cart {
-    border: 1px solid #ccc;
-    padding: 10px;
-    margin-top: 10px;
-}
+        /* Style for the shopping cart section */
+        #cart {
+            border: 1px solid #ccc;
+            padding: 10px;
+            margin-top: 10px;
+        }
 
-.cart-item {
-    display: flex;
-    align-items: center;
-    margin-bottom: 5px;
-}
+        .cart-item {
+            display: flex;
+            align-items: center;
+            margin-bottom: 5px;
+        }
 
-.cart-item img {
-    width: 50px;
-    height: 50px;
-    object-fit: cover;
-    margin-right: 10px;
-}
+        .cart-item img {
+            width: 50px;
+            height: 50px;
+            object-fit: cover;
+            margin-right: 10px;
+        }
 
-.cart-item p {
-    margin: 0;
-}
+        .cart-item p {
+            margin: 0;
+        }
 
-/* Style for links */
-a {
-    color: #007bff;
-    text-decoration: none;
-}
+        /* Style for links */
+        a {
+            color: #007bff;
+            text-decoration: none;
+        }
 
-a:hover {
-    text-decoration: underline;
-}
+        a:hover {
+            text-decoration: underline;
+        }
+        
+        /* Style for the login and cart buttons */
+        #login-button, #cart-button {
+            background-color: #007bff;
+            color: white;
+            border: none;
+            padding: 8px 16px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 14px;
+            cursor: pointer;
+            transition-duration: 0.4s;
+            margin-right: 10px;
+        }
 
+        #login-button:hover, #cart-button:hover {
+            background-color: #0056b3;
+        }
     </style>
 </head>
 <body>
     <h1>Welcome to the Shopping Application</h1>
+
+    <div>
+        <button id="login-button">Login</button>
+        <button id="cart-button">Cart</button>
+    </div>
+    
 
     <h2>Featured Products</h2>
     <div class="product">
@@ -128,9 +152,21 @@ a:hover {
         <button>Add to Cart</button>
     </div>
 
-    <h2>Shopping Cart</h2>
-    <div class="cart">
-      <button>CART</button>
-    </div>
+    <script>
+        // JavaScript code for handling button clicks
+        var loginButton = document.getElementById("login-button");
+        var cartButton = document.getElementById("cart-button");
+
+
+
+        loginButton.addEventListener("click", function() {
+        window.location.href = "https://php-login-test-project.apps.cluster-sbj27.sbj27.sandbox2370.opentlc.com/";
+            alert("Login button clicked!");
+        });
+
+        cartButton.addEventListener("click", function() {
+            alert("Cart button clicked!");
+        });
+    </script>
 </body>
 </html>
